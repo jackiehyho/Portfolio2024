30930b307410859bfb84b3cb9331ac7b0013629b
Jackie Ho <jackie.hy.ho@gmail.com>
2023-12-18 19:33:27 -0800

    Installed react and styled components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
+import sanity from "astro-sanity";
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sanity({
+    projectId: "vq908ksg",
+    dataset: "production"
+  }), react()],
+  vite: {
+    ssr: {
+      noExternal: ['node_modules']
+    }
+  }
+});
